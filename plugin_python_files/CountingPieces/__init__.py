41f5d5a53ee121e75ad383f4bf3a4f7b94878951
Mary Stirling Brown <marystirlingbrown@gmail.com>
2023-11-28 23:00:56 -0600

    updated miniproject

---
+++ plugin_python_files/CountingPieces/__init__.py
@@ -0,0 +1,53 @@
+"""
+This is where the implementation of the plugin code goes.
+The CountingPieces-class is imported from both run_plugin.py and run_debug.py
+"""
+import sys
+import logging
+from webgme_bindings import PluginBase
+
+# Setup a logger
+logger = logging.getLogger('CountingPieces')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)  # By default it logs to stderr..
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+class CountingPieces(PluginBase):
+  def main(self):
+    active_node = self.active_node
+    core = self.core
+    logger = self.logger
+    self.namespace = None
+    META = self.META
+    
+    nodesList = core.load_sub_tree(active_node)
+    nodes = {}
+    
+    boardNode = active_node
+    
+    # collect all nodes path
+    for node in nodesList:
+      nodes[core.get_path(node)] = node
+    
+    totalWhites = 0
+    totalBlacks = 0
+    
+    for node in nodesList:
+      if (core.is_instance_of(node, META['Tile'])):
+        tilePiece = core.get_children_paths(node)
+        tilePieceColor = "none"
+        # gets color of tile if there is a piece on that tile; if not, stays 'none'
+        for tilePieceNode in nodesList:
+          if len(tilePiece) > 0:
+            if tilePieceNode['nodePath'] == tilePiece[0]:
+              tilePieceColor = core.get_attribute(tilePieceNode, 'color')
+              if tilePieceColor == 'white':
+                totalWhites += 1
+              elif tilePieceColor == 'black':
+                totalBlacks += 1
+    logger.info('whites: {0}'.format(totalWhites))
+    logger.info('blacks: {0}'.format(totalBlacks))
--- src/common/utils.js
+++ src/common/utils.js
@@ -11,17 +11,12 @@ define(['./constants'], function (CONSTANTS) {
                 const column = Number(core.getAttribute(node, 'column'));
                 let value = CONSTANTS.PIECE.EMPTY;
                 const pieces = core.getChildrenPaths(node);
-                if (pieces.length > 0) {
-                    const pieceNode = nodeHash[pieces[0]];
-                    const color = core.getAttribute(pieceNode, 'color');
-                    
-                    if (color === 'black') {
-                        value = CONSTANTS.PIECE.BLACK;
-                    } else if (color === 'white') {
-                        value = CONSTANTS.PIECE.WHITE;
-                    }
+                if(pieces.length > 0) {
+                    value = core.isInstanceOf(nodeHash[pieces[0]], META.BlackPiece) ? 
+                        CONSTANTS.PIECE.BLACK : CONSTANTS.PIECE.WHITE;
                 }
-                board[row * 8 + column] = value;
+                board[row][column] = value
+                //board[row * 8 + column] = value;
             });
             return board;
         },
@@ -31,7 +26,8 @@ define(['./constants'], function (CONSTANTS) {
                 const node = nodeHash[tile];
                 const row = Number(core.getAttribute(node, 'row'));
                 const column = Number(core.getAttribute(node, 'column'));
-                hash[row * 8 + column] = core.getPath(node);
+                //hash[row * 8 + column] = core.getPath(node);
+                hash[row][column] = core.getPath(node);
             });
             return hash;
         }
